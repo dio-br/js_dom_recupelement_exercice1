dcfb6aec458608cf0e3e944e425e336cb270a9d4
Mohammed <el-med7@outlook.com>
2021-01-26 09:41:43 +0100

    tout est bon

---
+++ main.js
@@ -0,0 +1,33 @@
+let stock = document.getElementsByTagName('h1')
+console.log(stock);
+
+////
+let stock2 = document.getElementsByTagName('h3')
+console.log(stock2);
+
+let first = document.getElementsByTagName('h3')[0]
+console.log(first);
+
+let second = document.getElementsByTagName('h3')[1]
+console.log(second);
+
+let third = document.getElementsByTagName('h3')[2]
+console.log(third);
+
+
+////
+
+let stock3 = document.getElementsByTagName('p')
+console.log(stock3);
+
+let para = document.getElementsByTagName('p')[1]
+console.log(para);
+
+///
+
+let stock4 = document.getElementsByTagName('li')
+console.log(stock4);
+
+let list = document.getElementsByTagName('li')[2]
+console.log(list);
+
